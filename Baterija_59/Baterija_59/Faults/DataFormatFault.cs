c7d9dd0f5e7f996a4f87a640e7ffbaef0d1b4fd2
Dobreta22 <ivandobretic@gmail.com>
2026-05-05 18:17:47 +0200

    Dodati fault modeli za validaciju i format podataka

---
+++ Baterija_59/Baterija_59/Faults/DataFormatFault.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Baterija_59.Faults
+{
+    [DataContract]
+    public class DataFormatFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        public DataFormatFault()
+        {
+        }
+
+        public DataFormatFault(string message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ Baterija_59/Baterija_59/Faults/ValidationFault.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Baterija_59.Faults
+{
+    [DataContract]
+    public class ValidationFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string FieldName { get; set; }
+
+        public ValidationFault()
+        {
+        }
+
+        public ValidationFault(string message, string fieldName)
+        {
+            Message = message;
+            FieldName = fieldName;
+        }
+    }
+}
